5c4b675fe18b11abe03a8e2ed8897424de42c801
nathgaitan <nathalia2912dante@gmail.com>
2022-01-05 14:16:01 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import SideBar from './components/SideBar';
+import ContentWrapper from './components/ContentWrapper';
+
+function App() {
+  return (
+    <div id="wrapper">
+
+      <SideBar />
+
+      <ContentWrapper />
+
+    </div>
+  );
+}
+
+export default App;
